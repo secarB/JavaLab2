d15607d1b9fe9ffeb9df9ae2b209bf4eba9e3b2b
lepha <lepha@DESKTOP-MTBFI8L>
2020-12-01 21:35:49 +0300

    Up

--- src/Rest.java
+++ src/Rest.java
@@ -1,11 +1,18 @@
 import ru.ifmo.se.pokemon.*;
 
-public class Rest extends Move {
+public class Rest extends StatusMove {
+    public Rest(){
+        super(Type.PSYCHIC,0,0);
+    }
 
-	@Override
-	protected void attack(Pokemon arg0, Pokemon arg1) {
-		// TODO Auto-generated method stub
+    @Override
+    protected void applySelfEffects(Pokemon p){
+        p.addEffect( new Effect().condition(Status.SLEEP).turns(2));
+        p.setMod(Stat.HP, (int) p.getStat(Stat.HP));
+    }
 
-	}
-
+    @Override
+    protected  String describe(){
+        return "User sleeps for 2 turns, but user is fully healed.";
+    }
 }
